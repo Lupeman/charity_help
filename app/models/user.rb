b0c421e44c976e2fc26055e873bf8915712b3163
Tess Elation <fredisded@gmail.com>
2017-01-17 11:54:47 +1100

    add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :charities
   has_many :donors
 
+  has_secure_password
+
   before_save { self.username = username.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username, presence: true, length: { maximum: 255 },
