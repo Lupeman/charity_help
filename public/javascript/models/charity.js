f7293dbe6ddcbfe0a469d933e26b816344bc1fd8
Lupeman <jaimegunther@Jaimes-MacBook-Air.local>
2017-01-18 14:22:13 +1100

    create and implement template for charity search

--- public/javascript/models/charity.js
+++ public/javascript/models/charity.js
@@ -1,6 +1,6 @@
 var Charity = Backbone.Model.extend({
-  updateProps: funciton(element) {
-    this.set(element.className, element.textContent);
-    this.save();
-  }
+  // updateProps: function(element) {
+  //   this.set(element.className, element.textContent);
+  //   this.save();
+  // }
 })
--- public/javascript/templates.js
+++ public/javascript/templates.js
@@ -3,10 +3,14 @@
 templates['charity'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<h2 class=\"name\">"
+  return "<h2 class=\"name\"><a href=\"/charity/"
+    + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
+    + "\" class=\"profile-link\">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</h2>\n<p class=\"decription\">"
-    + alias4(((helper = (helper = helpers.decsription || (depth0 != null ? depth0.decsription : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"decsription","hash":{},"data":data}) : helper)))
-    + "</p>\n<hr/>\n";
+    + "</a></h2>\n<p class=\"description\">"
+    + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
+    + "</p>\n<img class=\"logo\" src=\""
+    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
+    + "\" />\n<br />\n";
 },"useData":true});
 })();
