a484d07a5f26def2a5858d4e6bf255a0109f83ae
Tess Elation <fredisded@gmail.com>
2017-01-19 20:11:31 +1100

    ensure only logged in users can see our page

--- public/javascript/templates.js
+++ public/javascript/templates.js
@@ -15,22 +15,16 @@ templates['charityprofile'] = template({"compiler":[7,">= 4.0.0"],"main":functio
   return "<div class=\"container\">\n\n  <div class=\"container\">\n    <h1 class=\"charity_name text-center\" contenteditable=\"true\"> "
     + alias4(((helper = (helper = helpers.charity_name || (depth0 != null ? depth0.charity_name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"charity_name","hash":{},"data":data}) : helper)))
     + " </h1>\n    <p class=\"text-center\"><a class=\"url\" contenteditable=\"true\" href=\""
-<<<<<<< HEAD
     + alias4(((helper = (helper = helpers.charity_website || (depth0 != null ? depth0.charity_website : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"charity_website","hash":{},"data":data}) : helper)))
     + "\"> "
     + alias4(((helper = (helper = helpers.charity_website || (depth0 != null ? depth0.charity_website : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"charity_website","hash":{},"data":data}) : helper)))
-=======
-    + alias1(((helper = (helper = helpers.charity_website || (depth0 != null ? depth0.charity_website : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"charity_website","hash":{},"data":data}) : helper)))
-    + "\"> "
-    + alias1(((helper = (helper = helpers.charity_website || (depth0 != null ? depth0.charity_website : depth0)) != null ? helper : alias3),(typeof helper === alias4 ? helper.call(alias2,{"name":"charity_website","hash":{},"data":data}) : helper)))
->>>>>>> cb25bb63e830c547d6f13e37735e101a642b7411
     + " </a></p>\n    <p class=\"description text-center\" contenteditable=\"true\"> "
     + alias4(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper)))
     + " </p>\n    <p class=\"shipping_address text-center\" contenteditable=\"true\"> "
     + alias4(((helper = (helper = helpers.shipping_address || (depth0 != null ? depth0.shipping_address : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"shipping_address","hash":{},"data":data}) : helper)))
     + " </p>\n  </div>\n\n  <div class=\"container\">\n    <div class=\"row\">\n      <div class=\"img-responsive text-center\">\n        <img src=\""
     + alias4(container.lambda(((stack1 = (depth0 != null ? depth0.logo : depth0)) != null ? stack1.url : stack1), depth0))
-    + "\" alt=\"charity_logo\" class=\"img-rounded center-block\">\n      </div>\n    </div>\n  </div>\n  \n</div>\n";
+    + "\" alt=\"charity_logo\" class=\"img-rounded center-block\">\n      </div>\n    </div>\n  </div>\n\n</div>\n";
 },"useData":true});
 templates['wish'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
