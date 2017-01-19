0312faeb15580f2f40510bfe9a42c4f295d5057c
Tya Cao <cao.tya@gmail.com>
2017-01-19 22:54:28 +1100

    update overlay menu with ajax - now showing 15 charities

--- app/assets/javascripts/landing_page.js
+++ app/assets/javascripts/landing_page.js
@@ -54,8 +54,23 @@ $(document).ready(function(){
 
 // Open overlay
 function openOverlay() {
+
+  var settings = {
+      url: '/charities',
+      method: 'get',
+      dataType: 'json'
+  };
+
+  $.ajax(settings).done(function(response) {
+    $('#overlay-content').empty();
+    var firstFifteenResults = _.first(response, 15);
+    firstFifteenResults.forEach(function(charity){
+    var toAppend = "<li><a href='http://" + charity["charity_website"] + "'>" + charity["charity_name"] + "<br><span class='type'>" + charity["cause"] + "</span></a><li>"
+    $("#overlay-content").append(toAppend);
+    })
     document.getElementById("charity-overlay").style.height = "100%";
-}
+  });
+};
 
 // Close overlay
 function closeOverlay() {
